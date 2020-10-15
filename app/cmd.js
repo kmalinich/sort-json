87442222d712d7927e175f385d2e80a885e8110f
Ken Malinich <kmalinich@users.noreply.github.com>
2020-10-15 16:19:32 -0400

    Update

--- app/cmd.js
+++ app/cmd.js
@@ -6,70 +6,139 @@ const path = require('path');
 // NPM dependencies
 const sortJson = require('./');
 
+const mmm   = require('mmmagic');
+const Magic = mmm.Magic;
+const magic = new Magic(mmm.MAGIC_MIME_TYPE);
 
 // JSON rcfile regexp
 const rcRegexp = /\..*.[rc]$/g;
 
-const args = process.argv.slice(0);
 
-// Get all the files
-const files = args.filter(arg => {
-	const argSkip0 = arg === '--ignore-case' || arg === '-i';
-	const argSkip1 = arg === '--reverse'     || arg === '-r';
+async function asyncRun() {
+	const args = process.argv.slice(0);
 
-	const argSkipAnyPt1 = (argSkip0 || argSkip1);
+	// Get all the files
+	const asyncFileFilter = async (fileArray, filterFunction) => {
+		const results = await Promise.all(fileArray.map(filterFunction));
+		return fileArray.filter((_v, index) => results[index]);
+	};
 
-	if (argSkipAnyPt1) return false;
+	const files = await asyncFileFilter(args, async (arg) => {
+		try {
+			const argSkip0 = arg === '--ignore-case' || arg === '-i';
+			const argSkip1 = arg === '--reverse'     || arg === '-r';
 
+			const argSkipAnyPt1 = (argSkip0 || argSkip1);
 
-	const argNormalized = path.normalize(arg);
+			// console.log({
+			// 	argInfo0 : {
+			// 		arg,
+			// 		argSkip0,
+			// 		argSkip1,
+			// 		argSkipAnyPt1,
+			// 	},
+			// });
 
-
-	const pathParse = path.parse(argNormalized);
-
-	const argSkip2 = (pathParse.root === '/' && pathParse.base === 'node');
-	const argSkip3 = (pathParse.root === '/' && pathParse.base === 'sort-json');
-	const argSkip4 = (pathParse.root === '/' && pathParse.base === 'cmd.js');
-
-	const argSkipAnyPt2 = (argSkip2 || argSkip3 || argSkip4);
-
-	if (argSkipAnyPt2) return false;
-
-
-	const argCheck0 = pathParse.ext === '.json';
-	const argCheck1 = pathParse.ext === '.rc';
-	const argCheck2 = pathParse.base === 'tiddlywiki.info';
-	const argCheck3 = pathParse.base === 'lovelace';
-	const argCheck4 = pathParse.base.startsWith('core.');
-	const argCheck5 = pathParse.base.startsWith('esphome.');
-	const argCheck6 = (pathParse.base.match(rcRegexp) !== null);
-
-	const argCheckAny = (argCheck0 || argCheck1 || argCheck2 || argCheck3 || argCheck4 || argCheck5 || argCheck6);
-
-	// console.log({
-	// 	argInfo : {
-	// 		arg, argNormalized,
-
-	// 		pathParse,
-
-	// 		argCheck0, argCheck1, argCheck2, argCheck3, argCheck4, argCheck5, argCheck6, argCheckAny,
-	// 	},
-	// });
-
-	return argCheckAny;
-});
+			if (argSkipAnyPt1) return false;
 
 
-// CLI arguments
-const options = {
-	ignoreCase : args.includes('--ignore-case') || args.includes('-i'),
-	reverse    : args.includes('--reverse')     || args.includes('-r'),
-};
+			const argNormalized = path.normalize(arg);
 
-if (files.length === 0) {
-	console.log('Error: No files to process, cannot continue');
-	process.exit(1);
+			const pathParse = path.parse(argNormalized);
+
+			const argSkip2 = (pathParse.root === '/' && pathParse.base === 'node');
+			const argSkip3 = (pathParse.root === '/' && pathParse.base === 'sort-json');
+			const argSkip4 = (pathParse.root === '/' && pathParse.base === 'cmd.js');
+
+			const argSkipAnyPt2 = (argSkip2 || argSkip3 || argSkip4);
+
+			// console.log({
+			// 	argInfo1 : {
+			// 		arg,
+			// 		argNormalized,
+			// 		pathParse,
+			// 		argSkip2,
+			// 		argSkip3,
+			// 		argSkip4,
+			// 		argSkipAnyPt2,
+			// 	},
+			// });
+
+			if (argSkipAnyPt2) return false;
+
+
+			const argCheck0 = (pathParse.base.match(rcRegexp) !== null);
+
+			const argCheck1 = pathParse.ext === '.json';
+			const argCheck2 = pathParse.ext === '.rc';
+
+			const argCheck3 = pathParse.base === 'composer.lock';
+			const argCheck4 = pathParse.base === 'tiddlywiki.info';
+			const argCheck5 = pathParse.base === 'lovelace';
+
+			const argCheck6 = pathParse.base.startsWith('core.');
+			const argCheck7 = pathParse.base.startsWith('esphome.');
+
+			const mimeType = await new Promise((resolve, reject) => {
+				magic.detectFile(argNormalized, (detectError, detectResult) => {
+					if (detectError !== null) return reject(detectError);
+					resolve(detectResult);
+				});
+			});
+
+			const argCheck8 = (mimeType === 'application/json');
+
+			const argCheckAny = (argCheck0 || argCheck1 || argCheck2 || argCheck3 || argCheck4 || argCheck5 || argCheck6 || argCheck7 || argCheck8);
+
+			// console.log({
+			// 	argInfo2 : {
+			// 		arg,
+			// 		argNormalized,
+
+			// 		mimeType,
+			// 		pathParse,
+
+			// 		argChecks : [
+			// 			argCheck0,
+			// 			argCheck1,
+			// 			argCheck2,
+			// 			argCheck3,
+			// 			argCheck4,
+			// 			argCheck5,
+			// 			argCheck6,
+			// 			argCheck7,
+			// 			argCheck8,
+			// 		],
+
+			// 		argCheckAny,
+			// 	},
+			// });
+
+			return argCheckAny;
+		}
+		catch (error) {
+			if (error === null) return false;
+			console.log('Error: Failure during file type/MIME type check, cannot continue');
+			throw error;
+		}
+	});
+
+
+	// CLI arguments
+	const options = {
+		ignoreCase : args.includes('--ignore-case') || args.includes('-i'),
+		reverse    : args.includes('--reverse')     || args.includes('-r'),
+	};
+
+	if (files.length === 0) {
+		console.log('Error: No files to process, cannot continue');
+		process.exit(1);
+	}
+
+	// console.log({ files });
+
+	sortJson.overwrite(files.map(f => path.resolve(f)), options);
 }
 
 
-sortJson.overwrite(files.map(f => path.resolve(f)), options);
+asyncRun();
